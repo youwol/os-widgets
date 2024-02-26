c7729958c74fda8710782add2339ad0d1cdc5198
jehadmelad <jd.melad@gmail.com>
2024-02-26 10:57:10 +0100

    ✨ template.py: PackageType.Library =>PackageType.LIBRARY

--- template.py
+++ template.py
@@ -25,7 +25,7 @@ project_externals = {
 }
 template = Template(
     path=folder_path,
-    type=PackageType.Library,
+    type=PackageType.LIBRARY,
     name=pkg_json['name'],
     version=pkg_json['version'],
     shortDescription=pkg_json['description'],
