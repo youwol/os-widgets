1ec15c75b518dd88e7172032b6939ce9573d7333
jehadmelad <jd.melad@gmail.com>
2024-02-26 11:01:07 +0100

    ✨  index.ts => expose `assetsWidget`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,7 @@ import type * as FavoriteModule from './favorites'
 import type * as WebpmPackageInfoModule from './webpm-package-info'
 import type * as FileInfoModule from './file-info'
 import type * as AboutYouwolModule from './about-youwol'
+import type * as AssetViewModule from './assets'
 import type { AssetLightDescription } from '@youwol/os-core'
 import type { AssetsBackend } from '@youwol/http-clients'
 
@@ -12,6 +13,7 @@ export type * as WebpmPackageInfoTypes from './webpm-package-info'
 export type * as FavoritesTypes from './favorites'
 export type * as FileInfoTypes from './file-info'
 export type * as AboutYouwolTypes from './about-youwol'
+export type * as AssetTypes from './assets/asset-card'
 
 export async function favoritesModule() {
     const module: typeof FavoriteModule = await setup.installAuxiliaryModule({
@@ -95,4 +97,25 @@ export async function aboutYouwolWidget({
         },
     )
     return new module.AboutView({ productName })
+}
+
+export async function assetViewModule() {
+    const module: typeof AssetViewModule = await setup.installAuxiliaryModule({
+        name: 'assets',
+        cdnClient: webpmClient,
+    })
+    return module
+}
+export async function assetsWidget({
+    asset,
+    permissions,
+}: {
+    asset: AssetsBackend.GetAssetResponse
+    permissions: AssetsBackend.GetPermissionsResponse
+}) {
+    const module: typeof AssetViewModule = await setup.installAuxiliaryModule({
+        name: 'assets',
+        cdnClient: webpmClient,
+    })
+    return new module.AssetView({ asset, permissions })
 }
