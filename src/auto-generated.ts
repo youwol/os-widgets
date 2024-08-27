ceef2785830d2833c89933a80dbf5b1eff3fd1a8
youwol-guillaume <greinisch@youwol.com>
2024-08-27 16:38:55 +0200

    🔧 Sync. auto-generated artifacts

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,35 +1,25 @@
 
 const runTimeDependencies = {
     "externals": {
-        "@youwol/os-core": "^0.2.0",
-        "@youwol/webpm-client": "^3.0.0",
         "@youwol/http-clients": "^3.0.0",
         "@youwol/http-primitives": "^0.2.0",
-        "@youwol/rx-vdom": "^1.0.1",
-        "@youwol/rx-group-views": "^0.3.0",
-        "@youwol/rx-tab-views": "^0.3.0",
-        "@youwol/rx-input-views": "^0.3.0",
+        "@youwol/os-core": "^0.2.0",
         "@youwol/rx-button-views": "^0.2.0",
         "@youwol/rx-context-menu-views": "^0.2.0",
+        "@youwol/rx-group-views": "^0.3.0",
+        "@youwol/rx-input-views": "^0.3.0",
+        "@youwol/rx-tab-views": "^0.3.0",
         "@youwol/rx-tree-views": "^0.3.3",
-        "rxjs": "^7.5.6",
+        "@youwol/rx-vdom": "^1.0.1",
+        "@youwol/webpm-client": "^3.0.0",
         "lodash": "^4.17.15",
-        "uuid": "^8.3.2",
-        "marked": "^4.2.3"
+        "marked": "^4.2.3",
+        "rxjs": "^7.5.6",
+        "uuid": "^8.3.2"
     },
     "includedInBundle": {}
 }
 const externals = {
-    "@youwol/os-core": {
-        "commonjs": "@youwol/os-core",
-        "commonjs2": "@youwol/os-core",
-        "root": "@youwol/os-core_APIv02"
-    },
-    "@youwol/webpm-client": {
-        "commonjs": "@youwol/webpm-client",
-        "commonjs2": "@youwol/webpm-client",
-        "root": "@youwol/webpm-client_APIv3"
-    },
     "@youwol/http-clients": {
         "commonjs": "@youwol/http-clients",
         "commonjs2": "@youwol/http-clients",
@@ -40,25 +30,10 @@ const externals = {
         "commonjs2": "@youwol/http-primitives",
         "root": "@youwol/http-primitives_APIv02"
     },
-    "@youwol/rx-vdom": {
-        "commonjs": "@youwol/rx-vdom",
-        "commonjs2": "@youwol/rx-vdom",
-        "root": "@youwol/rx-vdom_APIv1"
-    },
-    "@youwol/rx-group-views": {
-        "commonjs": "@youwol/rx-group-views",
-        "commonjs2": "@youwol/rx-group-views",
-        "root": "@youwol/rx-group-views_APIv03"
-    },
-    "@youwol/rx-tab-views": {
-        "commonjs": "@youwol/rx-tab-views",
-        "commonjs2": "@youwol/rx-tab-views",
-        "root": "@youwol/rx-tab-views_APIv03"
-    },
-    "@youwol/rx-input-views": {
-        "commonjs": "@youwol/rx-input-views",
-        "commonjs2": "@youwol/rx-input-views",
-        "root": "@youwol/rx-input-views_APIv03"
+    "@youwol/os-core": {
+        "commonjs": "@youwol/os-core",
+        "commonjs2": "@youwol/os-core",
+        "root": "@youwol/os-core_APIv02"
     },
     "@youwol/rx-button-views": {
         "commonjs": "@youwol/rx-button-views",
@@ -70,30 +45,50 @@ const externals = {
         "commonjs2": "@youwol/rx-context-menu-views",
         "root": "@youwol/rx-context-menu-views_APIv02"
     },
+    "@youwol/rx-group-views": {
+        "commonjs": "@youwol/rx-group-views",
+        "commonjs2": "@youwol/rx-group-views",
+        "root": "@youwol/rx-group-views_APIv03"
+    },
+    "@youwol/rx-input-views": {
+        "commonjs": "@youwol/rx-input-views",
+        "commonjs2": "@youwol/rx-input-views",
+        "root": "@youwol/rx-input-views_APIv03"
+    },
+    "@youwol/rx-tab-views": {
+        "commonjs": "@youwol/rx-tab-views",
+        "commonjs2": "@youwol/rx-tab-views",
+        "root": "@youwol/rx-tab-views_APIv03"
+    },
     "@youwol/rx-tree-views": {
         "commonjs": "@youwol/rx-tree-views",
         "commonjs2": "@youwol/rx-tree-views",
         "root": "@youwol/rx-tree-views_APIv03"
     },
-    "rxjs": {
-        "commonjs": "rxjs",
-        "commonjs2": "rxjs",
-        "root": "rxjs_APIv7"
+    "@youwol/rx-vdom": {
+        "commonjs": "@youwol/rx-vdom",
+        "commonjs2": "@youwol/rx-vdom",
+        "root": "@youwol/rx-vdom_APIv1"
+    },
+    "@youwol/webpm-client": {
+        "commonjs": "@youwol/webpm-client",
+        "commonjs2": "@youwol/webpm-client",
+        "root": "@youwol/webpm-client_APIv3"
     },
     "lodash": {
         "commonjs": "lodash",
         "commonjs2": "lodash",
-        "root": "__APIv4"
-    },
-    "uuid": {
-        "commonjs": "uuid",
-        "commonjs2": "uuid",
-        "root": "uuid_APIv8"
+        "root": "lodash_APIv4"
     },
     "marked": {
         "commonjs": "marked",
         "commonjs2": "marked",
         "root": "marked_APIv4"
+    },
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv7"
     },
     "rxjs/operators": {
         "commonjs": "rxjs/operators",
@@ -102,17 +97,14 @@ const externals = {
             "rxjs_APIv7",
             "operators"
         ]
+    },
+    "uuid": {
+        "commonjs": "uuid",
+        "commonjs2": "uuid",
+        "root": "uuid_APIv8"
     }
 }
 const exportedSymbols = {
-    "@youwol/os-core": {
-        "apiKey": "02",
-        "exportedSymbol": "@youwol/os-core"
-    },
-    "@youwol/webpm-client": {
-        "apiKey": "3",
-        "exportedSymbol": "@youwol/webpm-client"
-    },
     "@youwol/http-clients": {
         "apiKey": "3",
         "exportedSymbol": "@youwol/http-clients"
@@ -121,21 +113,9 @@ const exportedSymbols = {
         "apiKey": "02",
         "exportedSymbol": "@youwol/http-primitives"
     },
-    "@youwol/rx-vdom": {
-        "apiKey": "1",
-        "exportedSymbol": "@youwol/rx-vdom"
-    },
-    "@youwol/rx-group-views": {
-        "apiKey": "03",
-        "exportedSymbol": "@youwol/rx-group-views"
-    },
-    "@youwol/rx-tab-views": {
-        "apiKey": "03",
-        "exportedSymbol": "@youwol/rx-tab-views"
-    },
-    "@youwol/rx-input-views": {
-        "apiKey": "03",
-        "exportedSymbol": "@youwol/rx-input-views"
+    "@youwol/os-core": {
+        "apiKey": "02",
+        "exportedSymbol": "@youwol/os-core"
     },
     "@youwol/rx-button-views": {
         "apiKey": "02",
@@ -145,25 +125,45 @@ const exportedSymbols = {
         "apiKey": "02",
         "exportedSymbol": "@youwol/rx-context-menu-views"
     },
+    "@youwol/rx-group-views": {
+        "apiKey": "03",
+        "exportedSymbol": "@youwol/rx-group-views"
+    },
+    "@youwol/rx-input-views": {
+        "apiKey": "03",
+        "exportedSymbol": "@youwol/rx-input-views"
+    },
+    "@youwol/rx-tab-views": {
+        "apiKey": "03",
+        "exportedSymbol": "@youwol/rx-tab-views"
+    },
     "@youwol/rx-tree-views": {
         "apiKey": "03",
         "exportedSymbol": "@youwol/rx-tree-views"
+    },
+    "@youwol/rx-vdom": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/rx-vdom"
+    },
+    "@youwol/webpm-client": {
+        "apiKey": "3",
+        "exportedSymbol": "@youwol/webpm-client"
+    },
+    "lodash": {
+        "apiKey": "4",
+        "exportedSymbol": "lodash"
+    },
+    "marked": {
+        "apiKey": "4",
+        "exportedSymbol": "marked"
     },
     "rxjs": {
         "apiKey": "7",
         "exportedSymbol": "rxjs"
     },
-    "lodash": {
-        "apiKey": "4",
-        "exportedSymbol": "_"
-    },
     "uuid": {
         "apiKey": "8",
         "exportedSymbol": "uuid"
-    },
-    "marked": {
-        "apiKey": "4",
-        "exportedSymbol": "marked"
     }
 }
 
@@ -175,40 +175,6 @@ const mainEntry : {entryFile: string,loadDependencies:string[]} = {
 }
 
 const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {
-    "favorites": {
-        "entryFile": "./lib/favorites/index.ts",
-        "loadDependencies": [
-            "@youwol/webpm-client",
-            "@youwol/rx-vdom",
-            "rxjs",
-            "@youwol/http-clients",
-            "@youwol/os-core",
-            "@youwol/rx-group-views"
-        ],
-        "name": "favorites"
-    },
-    "webpm-package-info": {
-        "entryFile": "./lib/webpm-package-info/index.ts",
-        "loadDependencies": [
-            "@youwol/webpm-client",
-            "@youwol/rx-vdom",
-            "rxjs",
-            "@youwol/http-clients",
-            "@youwol/http-primitives",
-            "@youwol/rx-input-views"
-        ],
-        "name": "webpm-package-info"
-    },
-    "file-info": {
-        "entryFile": "./lib/file-info/index.ts",
-        "loadDependencies": [
-            "@youwol/rx-vdom",
-            "rxjs",
-            "@youwol/http-clients",
-            "@youwol/http-primitives"
-        ],
-        "name": "file-info"
-    },
     "about-youwol": {
         "entryFile": "./lib/about-youwol/index.ts",
         "loadDependencies": [
@@ -242,6 +208,40 @@ const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDepen
             "@youwol/rx-tree-views"
         ],
         "name": "explorer"
+    },
+    "favorites": {
+        "entryFile": "./lib/favorites/index.ts",
+        "loadDependencies": [
+            "@youwol/webpm-client",
+            "@youwol/rx-vdom",
+            "rxjs",
+            "@youwol/http-clients",
+            "@youwol/os-core",
+            "@youwol/rx-group-views"
+        ],
+        "name": "favorites"
+    },
+    "file-info": {
+        "entryFile": "./lib/file-info/index.ts",
+        "loadDependencies": [
+            "@youwol/rx-vdom",
+            "rxjs",
+            "@youwol/http-clients",
+            "@youwol/http-primitives"
+        ],
+        "name": "file-info"
+    },
+    "webpm-package-info": {
+        "entryFile": "./lib/webpm-package-info/index.ts",
+        "loadDependencies": [
+            "@youwol/webpm-client",
+            "@youwol/rx-vdom",
+            "rxjs",
+            "@youwol/http-clients",
+            "@youwol/http-primitives",
+            "@youwol/rx-input-views"
+        ],
+        "name": "webpm-package-info"
     }
 }
 
